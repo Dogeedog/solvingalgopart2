c0bf6674d23da1f02ec8e5d1eb31e94660d7fdb8
Dogeedog <jeimspolemiko@hotmail.com>
2022-07-02 00:29:05 -0500

    solves thingy

--- app/src/main/java/com/example/linearplexsolver/MainActivity.java
+++ app/src/main/java/com/example/linearplexsolver/MainActivity.java
@@ -12,13 +12,16 @@ import android.text.Html;
 import android.view.View;
 import android.webkit.WebView;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.NumberPicker;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     NumberPicker choices;
+    EditText nvalue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,27 +30,68 @@ public class MainActivity extends AppCompatActivity {
         choices = findViewById(R.id.numberepicker);
         String[] choicesstrings = {
                 "1", "2"
-        };
+        }; //string de 1 y 2 ?XD
         ((TextView)findViewById(R.id.textView)).setText(Html.fromHtml("y=B<sub><small>o</small></sub>+B<sub><small>1</small></sub>" +
-                "X<sub><small>1</small></sub>"));
+                "X<sub><small>1</small></sub>")); //asigna texto a la view donde va el modelo
 
+        choices.setDisplayedValues(choicesstrings);
+        choices.setMinValue(0); //index value 0:"1" - 1:"2"
+        choices.setMaxValue(choicesstrings.length - 1); //no idea
+        /*AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO); //no night mode
+        *
+        *
+
+         */
+        //cambia el modelo en base a la cantidad de variables
         choices.setOnValueChangedListener((picker, oldVal, newVal) -> {
             int OneOrTwoVar = choices.getValue();
             if (OneOrTwoVar == 0){
+                //Para 1 entrada y 1 de respuesta
                 ((TextView)findViewById(R.id.textView)).setText(Html.fromHtml("y=B<sub><small>o</small></sub>+B<sub><small>1</small></sub>" +
                         "X<sub><small>1</small></sub>"));
+                nvalue = findViewById(R.id.nnumber);
+                nvalue.setOnFocusChangeListener((v, hasFocus) -> {
+                    if(!hasFocus) {
+                        String nvaluestr = nvalue.getText().toString(); //obtener cantidad de filas
+                        int nvalueint;
+                        try {
+                            nvalueint = Integer.parseInt(nvaluestr);
+                            if (nvalueint >= 4){
+                                Toast.makeText(MainActivity.this, "my Message", Toast.LENGTH_LONG).show();
+                            }else{
+                                return;
+                            }
+                        }
+                        catch(NumberFormatException ex) {
+                            return;
+                        }
+                    }
+                });
             } else {
+                //Para 2 entradas y una respuesta
                 ((TextView)findViewById(R.id.textView)).setText(Html.fromHtml("y=B<sub><small>o</small></sub>+B<sub><small>1</small></sub>" +
                         "X<sub><small>1</small></sub>+B<sub><small>2</small></sub>X<sub><small>2</small></sub>"));
+                nvalue = findViewById(R.id.nnumber);
+                nvalue.setOnFocusChangeListener((v, hasFocus) -> {
+                    if(!hasFocus) {
+                        String nvaluestr = nvalue.getText().toString(); //obtener cantidad de filas
+                        int nvalueint;
+                        try {
+                            nvalueint = Integer.parseInt(nvaluestr);
+                            if (nvalueint >= 4){
+                                Toast.makeText(MainActivity.this, "my Message", Toast.LENGTH_LONG).show();
+                            }else{
+                                return;
+                            }
+                        }
+                        catch(NumberFormatException ex) {
+                            return;
+                        }
+                    }
+                });
             }
         });
-        choices.setDisplayedValues(choicesstrings);
-        choices.setMinValue(0);
-        choices.setMaxValue(choicesstrings.length - 1);
-        /*AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
-        *
-        *
-        * */
+
 
     }
 
