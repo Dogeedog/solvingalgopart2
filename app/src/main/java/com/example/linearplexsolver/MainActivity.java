a9dc51bb9cd3c1ef35d8d6f6e20677e5a76a49ee
Dogeedog <jeimspolemiko@hotmail.com>
2022-07-02 16:35:30 -0500

    solves thingy

--- app/src/main/java/com/example/linearplexsolver/MainActivity.java
+++ app/src/main/java/com/example/linearplexsolver/MainActivity.java
@@ -9,6 +9,7 @@ import android.graphics.PorterDuff;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.text.Html;
+import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebView;
@@ -29,13 +30,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         choices = findViewById(R.id.numberepicker);
         String[] choicesstrings = {
-                "1", "2"
+                "0", "1", "2"
         }; //string de 1 y 2 ?XD
-        ((TextView)findViewById(R.id.textView)).setText(Html.fromHtml("y=B<sub><small>o</small></sub>+B<sub><small>1</small></sub>" +
-                "X<sub><small>1</small></sub>")); //asigna texto a la view donde va el modelo
-
+        ((TextView)findViewById(R.id.textView)).setText(Html.fromHtml("y=B<sub><small>o</small></sub>")); //asigna texto a la view donde va el modelo
+        ((TextView)findViewById(R.id.textView)).setEnabled(false);
         choices.setDisplayedValues(choicesstrings);
         choices.setMinValue(0); //index value 0:"1" - 1:"2"
         choices.setMaxValue(choicesstrings.length - 1); //no idea
@@ -47,38 +48,54 @@ public class MainActivity extends AppCompatActivity {
         //cambia el modelo en base a la cantidad de variables
         choices.setOnValueChangedListener((picker, oldVal, newVal) -> {
             int OneOrTwoVar = choices.getValue();
-            if (OneOrTwoVar == 0){
+            if (OneOrTwoVar == 1){
                 //Para 1 entrada y 1 de respuesta
                 ((TextView)findViewById(R.id.textView)).setText(Html.fromHtml("y=B<sub><small>o</small></sub>+B<sub><small>1</small></sub>" +
                         "X<sub><small>1</small></sub>"));
                 nvalue = findViewById(R.id.nnumber);
                 nvalue.setOnFocusChangeListener((v, hasFocus) -> {
                     if(!hasFocus) {
-                        String nvaluestr = nvalue.getText().toString(); //obtener cantidad de filas
+                        String nvaluestr = nvalue.getText().toString(); //obtener cantidad de filas TO DO: limpiar las layout para volver a poner views y no se acumulen
                         int nvalueint;
                         try {
                             nvalueint = Integer.parseInt(nvaluestr);
                             if (nvalueint >= 4){
-                                /* to do
-                                Toast.makeText(MainActivity.this, "my Message", Toast.LENGTH_LONG).show();
-                                LinearLayout layout = (LinearLayout) findViewById(R.id.CAMBIARLAYOUT); // combinar 1 linear layout vertical con 4 horizontales
+                                Toast.makeText(MainActivity.this, "1var", Toast.LENGTH_LONG).show();
+                                LinearLayout layout = (LinearLayout) findViewById(R.id.nlayout); // combinar 1 linear layout vertical con 4 horizontales
+                                LinearLayout layouty = (LinearLayout) findViewById(R.id.ylayout);
+                                LinearLayout layoutxa = (LinearLayout) findViewById(R.id.xalayout);
+                                LinearLayout layoutxb = (LinearLayout) findViewById(R.id.xblayout);
 
-                                String[] items = {"item 1", "item 2", "item 3", "item 4", "item 5"}; // hacerlo con el n value
-
-                                for (int i = 0; i < items.length; i++) {
+                                for (int i = 1; i <= nvalueint; i++) {
                                     Button btn = new Button(this);
-                                    btn.setText(items[i]);
+                                    btn.setText((String.valueOf(i)));
                                     btn.setId(i);
-                                    LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
-                                            ViewGroup.LayoutParams.MATCH_PARENT,
-                                            ViewGroup.LayoutParams.WRAP_CONTENT);
-
-                                    params.setMargins(10, 10, 10, 10);
+                                    btn.setBackgroundResource(R.drawable.rect3);
+                                    btn.setGravity(Gravity.CENTER | Gravity.CENTER_VERTICAL | Gravity.CENTER_HORIZONTAL);
+                                    int width = (int) getResources().getDimension(R.dimen.width); //obtiene manualmente el width 58 de dimens
+                                    int height = (int) getResources().getDimension(R.dimen.height); //obtiene manualmente el height 35 de dimens
+                                    int topmargin = (int) getResources().getDimension(R.dimen.margin);
+                                    LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(width, height); //58, 35
+                                    params.setMargins(0, topmargin, 0, 0);
                                     btn.setLayoutParams(params);
                                     layout.addView(btn);
                                 }
-                                */
 
+
+                                for (int i = 1; i <= nvalueint; i++) {
+                                    EditText btn = new EditText(this);
+                                    btn.setText((String.valueOf(i)));
+                                    btn.setId(i);
+                                    btn.setBackgroundResource(R.drawable.rect);
+                                    btn.setGravity(Gravity.CENTER | Gravity.CENTER_VERTICAL | Gravity.CENTER_HORIZONTAL);
+                                    int width = (int) getResources().getDimension(R.dimen.width); //obtiene manualmente el width 58 de dimens
+                                    int height = (int) getResources().getDimension(R.dimen.height); //obtiene manualmente el height 35 de dimens
+                                    int topmargin = (int) getResources().getDimension(R.dimen.margin);
+                                    LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(width, height); //58, 35
+                                    params.setMargins(0, topmargin, 0, 0);
+                                    btn.setLayoutParams(params);
+                                    layouty.addView(btn);
+                                }
                             }else{
                                 return;
                             }
@@ -88,7 +105,7 @@ public class MainActivity extends AppCompatActivity {
                         }
                     }
                 });
-            } else {
+            } else if (OneOrTwoVar == 2){
                 //Para 2 entradas y una respuesta
                 ((TextView)findViewById(R.id.textView)).setText(Html.fromHtml("y=B<sub><small>o</small></sub>+B<sub><small>1</small></sub>" +
                         "X<sub><small>1</small></sub>+B<sub><small>2</small></sub>X<sub><small>2</small></sub>"));
@@ -100,7 +117,7 @@ public class MainActivity extends AppCompatActivity {
                         try {
                             nvalueint = Integer.parseInt(nvaluestr);
                             if (nvalueint >= 4){
-                                Toast.makeText(MainActivity.this, "my Message", Toast.LENGTH_LONG).show();
+                                Toast.makeText(MainActivity.this, "2var", Toast.LENGTH_LONG).show();
                             }else{
                                 return;
                             }
@@ -110,9 +127,11 @@ public class MainActivity extends AppCompatActivity {
                         }
                     }
                 });
+            } else {
+                ((TextView)findViewById(R.id.textView)).setText(Html.fromHtml("y=B<sub><small>o</small></sub>"));
+                Toast.makeText(MainActivity.this, "0var", Toast.LENGTH_LONG).show();
             }
         });
-
 
     }
 
@@ -121,4 +140,5 @@ public class MainActivity extends AppCompatActivity {
         Intent j = new Intent(this, solved.class);
         startActivity(j);
     }
+
 }
