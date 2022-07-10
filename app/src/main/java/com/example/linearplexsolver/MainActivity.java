f459d3a21b44167c14cef4f1a08bbb6e7f673f18
Dogeedog <jeimspolemiko@hotmail.com>
2022-07-09 22:29:30 -0500

    solves thingy

--- app/src/main/java/com/example/linearplexsolver/MainActivity.java
+++ app/src/main/java/com/example/linearplexsolver/MainActivity.java
@@ -1,22 +1,14 @@
 package com.example.linearplexsolver;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.app.AppCompatDelegate;
-
 import android.content.Intent;
 import android.graphics.Color;
-import android.graphics.PorterDuff;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.text.Html;
 import android.text.InputType;
 import android.view.Gravity;
 import android.view.View;
-import android.view.ViewGroup;
-import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.NumberPicker;
 import android.widget.TextView;
@@ -35,19 +27,14 @@ public class MainActivity extends AppCompatActivity {
         choices = findViewById(R.id.numberepicker);
         String[] choicesstrings = {
                 "0", "1", "2"
-        }; //string de 1 y 2 ?XD
+        };
         ((TextView)findViewById(R.id.textView)).setText(Html.fromHtml("y=B<sub><small>o</small></sub>")); //asigna texto a la view donde va el modelo
-        ((TextView)findViewById(R.id.textView)).setEnabled(false);
+        (findViewById(R.id.textView)).setEnabled(false);
         choices.setDisplayedValues(choicesstrings);
-        choices.setMinValue(0); //index value 0:"1" - 1:"2"
+        choices.setMinValue(0); //index value 0:"0" - 1:"1"...
         choices.setMaxValue(choicesstrings.length - 1); //no idea
-        /*AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO); //no night mode
-        *
-        *
 
-         */
         //cambia el modelo en base a la cantidad de variables
-
         ((TextView)findViewById(R.id.mean1)).setHint(Html.fromHtml("x<sub><small>1</small></sub>"));
         ((TextView)findViewById(R.id.mean2)).setHint(Html.fromHtml("x<sub><small>2</small></sub>"));
         ((TextView)findViewById(R.id.pre1)).setHint(Html.fromHtml("x<sub><small>1</small></sub>"));
@@ -56,6 +43,7 @@ public class MainActivity extends AppCompatActivity {
         ((TextView)findViewById(R.id.xacol)).setText(Html.fromHtml("x<sub><small>1</small></sub>"));
         ((TextView)findViewById(R.id.xbcol)).setText(Html.fromHtml("x<sub><small>2</small></sub>"));
 
+
         choices.setOnValueChangedListener((picker, oldVal, newVal) -> {
             int OneOrTwoVar = choices.getValue();
             if (OneOrTwoVar == 1){
@@ -63,18 +51,20 @@ public class MainActivity extends AppCompatActivity {
                 ((TextView)findViewById(R.id.textView)).setText(Html.fromHtml("y=B<sub><small>o</small></sub>+B<sub><small>1</small></sub>" +
                         "X<sub><small>1</small></sub>"));
                 nvalue = findViewById(R.id.nnumber);
+                nvalue.getText().clear();
                 nvalue.setOnFocusChangeListener((v, hasFocus) -> {
                     if(!hasFocus) {
                         String nvaluestr = nvalue.getText().toString(); //obtener cantidad de filas
                         int nvalueint;
                         try {
                             nvalueint = Integer.parseInt(nvaluestr);
+                            float[][] a = new float[nvalueint][2];
                             if (nvalueint >= 4){
                                 Toast.makeText(MainActivity.this, "1var", Toast.LENGTH_LONG).show();
-                                LinearLayout layout = (LinearLayout) findViewById(R.id.nlayout);
-                                LinearLayout layouty = (LinearLayout) findViewById(R.id.ylayout);
-                                LinearLayout layoutxa = (LinearLayout) findViewById(R.id.xalayout);
-                                LinearLayout layoutxb = (LinearLayout) findViewById(R.id.xblayout);
+                                LinearLayout layout = findViewById(R.id.nlayout);
+                                LinearLayout layouty = findViewById(R.id.ylayout);
+                                LinearLayout layoutxa = findViewById(R.id.xalayout);
+                                LinearLayout layoutxb = findViewById(R.id.xblayout);
                                 layout.removeAllViews();
                                 layouty.removeAllViews();
                                 layoutxa.removeAllViews();
@@ -98,6 +88,7 @@ public class MainActivity extends AppCompatActivity {
                                     layout.addView(btn);
 
                                 }
+
                                 //for para n edittexts(y)
                                 for (int i = 1; i <= nvalueint; i++) {
                                     int width = (int) getResources().getDimension(R.dimen.widthinput); //obtiene manualmente el width 58 de dimens
@@ -106,8 +97,9 @@ public class MainActivity extends AppCompatActivity {
                                     int textsize = (int) getResources().getDimension(R.dimen.text); //text size 15dp
                                     EditText btn = new EditText(this);
                                     btn.setTextSize(textsize);
+                                    String butag = "y"+i;
+                                    btn.setTag(butag);
                                     btn.setId(i);
-                                    btn.setEms(15);
                                     btn.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_DECIMAL | InputType.TYPE_NUMBER_FLAG_SIGNED);
                                     btn.setBackgroundResource(R.drawable.rect);
                                     btn.setGravity(Gravity.CENTER | Gravity.CENTER_VERTICAL | Gravity.CENTER_HORIZONTAL);
@@ -124,8 +116,9 @@ public class MainActivity extends AppCompatActivity {
                                     int textsize = (int) getResources().getDimension(R.dimen.text); //text size 15dp
                                     EditText btn = new EditText(this);
                                     btn.setTextSize(textsize);
-                                    int pepega = View.generateViewId();
-                                    btn.setId(pepega);
+                                    String butag = "xa"+i;
+                                    btn.setTag(butag);
+                                    btn.setId(i);
                                     btn.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_DECIMAL | InputType.TYPE_NUMBER_FLAG_SIGNED);
                                     btn.setBackgroundResource(R.drawable.rect);
                                     btn.setGravity(Gravity.CENTER | Gravity.CENTER_VERTICAL | Gravity.CENTER_HORIZONTAL);
@@ -134,8 +127,35 @@ public class MainActivity extends AppCompatActivity {
                                     btn.setLayoutParams(params);
                                     layoutxa.addView(btn);
                                 }
-
-
+                                Button tebtn = findViewById(R.id.btntest);
+                                tebtn.setOnClickListener(v1 -> {
+                                    //(y) for para encontrar cada boton por tag
+                                    for (int i=1; i <= nvalueint; i++) {
+                                        String butagy = "y"+i;
+                                        EditText content = layouty.findViewWithTag(butagy);
+                                        String contentstring = content.getText().toString();
+                                            try {
+                                                float contentint = Float.parseFloat(contentstring);
+                                                a[i-1][0] = contentint;
+                                                System.out.println(a[i-1][0]);
+                                        } catch(NumberFormatException ex) {
+                                            return;
+                                        }
+                                    }
+                                    //(xa) for para encontrar cada boton por tag
+                                    for (int i=1; i <= nvalueint; i++) {
+                                        String butagxa = "xa"+i;
+                                        EditText content = layoutxa.findViewWithTag(butagxa);
+                                        String contentstring = content.getText().toString();
+                                        try {
+                                            float contentint = Float.parseFloat(contentstring);
+                                            a[i-1][1] = contentint;
+                                            System.out.println(a[i-1][1]);
+                                        } catch(NumberFormatException ex) {
+                                            return;
+                                        }
+                                    }
+                                });
                             }else{
                                 return;
                             }
@@ -150,18 +170,20 @@ public class MainActivity extends AppCompatActivity {
                 ((TextView)findViewById(R.id.textView)).setText(Html.fromHtml("y=B<sub><small>o</small></sub>+B<sub><small>1</small></sub>" +
                         "X<sub><small>1</small></sub>+B<sub><small>2</small></sub>X<sub><small>2</small></sub>"));
                 nvalue = findViewById(R.id.nnumber);
+                nvalue.getText().clear();
                 nvalue.setOnFocusChangeListener((v, hasFocus) -> {
                     if(!hasFocus) {
                         String nvaluestr = nvalue.getText().toString(); //obtener cantidad de filas
                         int nvalueint;
                         try {
                             nvalueint = Integer.parseInt(nvaluestr);
+                            float[][] b = new float[nvalueint][3];
                             if (nvalueint >= 4){
-                                Toast.makeText(MainActivity.this, "1var", Toast.LENGTH_LONG).show();
-                                LinearLayout layout = (LinearLayout) findViewById(R.id.nlayout);
-                                LinearLayout layouty = (LinearLayout) findViewById(R.id.ylayout);
-                                LinearLayout layoutxa = (LinearLayout) findViewById(R.id.xalayout);
-                                LinearLayout layoutxb = (LinearLayout) findViewById(R.id.xblayout);
+                                Toast.makeText(MainActivity.this, "2var", Toast.LENGTH_LONG).show();
+                                LinearLayout layout = findViewById(R.id.nlayout);
+                                LinearLayout layouty = findViewById(R.id.ylayout);
+                                LinearLayout layoutxa = findViewById(R.id.xalayout);
+                                LinearLayout layoutxb = findViewById(R.id.xblayout);
                                 layout.removeAllViews();
                                 layouty.removeAllViews();
                                 layoutxa.removeAllViews();
@@ -192,6 +214,8 @@ public class MainActivity extends AppCompatActivity {
                                     int textsize = (int) getResources().getDimension(R.dimen.text); //text size 15dp
                                     EditText btn = new EditText(this);
                                     btn.setTextSize(textsize);
+                                    String butag = "ytwo"+i;
+                                    btn.setTag(butag);
                                     btn.setId(i);
                                     btn.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_DECIMAL | InputType.TYPE_NUMBER_FLAG_SIGNED);
                                     btn.setBackgroundResource(R.drawable.rect);
@@ -209,6 +233,8 @@ public class MainActivity extends AppCompatActivity {
                                     int textsize = (int) getResources().getDimension(R.dimen.text); //text size 15dp
                                     EditText btn = new EditText(this);
                                     btn.setTextSize(textsize);
+                                    String butag = "xatwo"+i;
+                                    btn.setTag(butag);
                                     btn.setId(i);
                                     btn.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_DECIMAL | InputType.TYPE_NUMBER_FLAG_SIGNED);
                                     btn.setBackgroundResource(R.drawable.rect);
@@ -226,6 +252,8 @@ public class MainActivity extends AppCompatActivity {
                                     int textsize = (int) getResources().getDimension(R.dimen.text); //text size 15dp
                                     EditText btn = new EditText(this);
                                     btn.setTextSize(textsize);
+                                    String butag = "xbtwo"+i;
+                                    btn.setTag(butag);
                                     btn.setId(i);
                                     btn.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_DECIMAL | InputType.TYPE_NUMBER_FLAG_SIGNED);
                                     btn.setBackgroundResource(R.drawable.rect);
@@ -235,6 +263,48 @@ public class MainActivity extends AppCompatActivity {
                                     btn.setLayoutParams(params);
                                     layoutxb.addView(btn);
                                 }
+                                Button tebtn = findViewById(R.id.btntest);
+                                tebtn.setOnClickListener(v12 -> {
+                                    //(y) for para encontrar cada boton por tag
+                                    for (int i=1; i <= nvalueint; i++) {
+                                        String butagy = "ytwo"+i;
+                                        EditText content = layouty.findViewWithTag(butagy);
+                                        String contentstring = content.getText().toString();
+                                        try {
+                                            float contentint = Float.parseFloat(contentstring);
+                                            b[i-1][0] = contentint;
+                                            System.out.println(b[i-1][0]);
+                                        } catch(NumberFormatException ex) {
+                                            return;
+                                        }
+                                    }
+                                    //(xa) for para encontrar cada boton por tag
+                                    for (int i=1; i <= nvalueint; i++) {
+                                        String butagxa = "xatwo"+i;
+                                        EditText content = layoutxa.findViewWithTag(butagxa);
+                                        String contentstring = content.getText().toString();
+                                        try {
+                                            float contentint = Float.parseFloat(contentstring);
+                                            b[i-1][1] = contentint;
+                                            System.out.println(b[i-1][1]);
+                                        } catch(NumberFormatException ex) {
+                                            return;
+                                        }
+                                    }
+                                    //(xb) for para encontrar cada boton por tag
+                                    for (int i=1; i <= nvalueint; i++) {
+                                        String butagxa = "xbtwo"+i;
+                                        EditText content = layoutxb.findViewWithTag(butagxa);
+                                        String contentstring = content.getText().toString();
+                                        try {
+                                            float contentint = Float.parseFloat(contentstring);
+                                            b[i-1][1] = contentint;
+                                            System.out.println(b[i-1][1]);
+                                        } catch(NumberFormatException ex) {
+                                            return;
+                                        }
+                                    }
+                                });
                             }else{
                                 return;
                             }
@@ -249,7 +319,6 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, "0var", Toast.LENGTH_LONG).show();
             }
         });
-
     }
 
     public void solver(View h){
@@ -257,5 +326,4 @@ public class MainActivity extends AppCompatActivity {
         Intent j = new Intent(this, solved.class);
         startActivity(j);
     }
-
 }
