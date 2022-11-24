61703cd712bc36868dba2120b63459e1089ad470
Dogeedog <jeimspolemiko@hotmail.com>
2022-11-24 10:33:58 -0600

    17 11 22 v2: *[doe] nuevos calculos de intervalos de confianza (dinamicos) en resultados *[doe] seccion de lsd en resultados

--- app/src/main/java/com/example/linearplexsolver/solvedDOEoneFactor.java
+++ app/src/main/java/com/example/linearplexsolver/solvedDOEoneFactor.java
@@ -1,15 +1,21 @@
 package com.example.linearplexsolver;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.text.Html;
 import android.text.InputType;
+import android.util.Log;
 import android.view.Gravity;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -18,10 +24,15 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 import org.hipparchus.distribution.continuous.TDistribution;
+import org.hipparchus.util.Combinations;
 import org.jgrapht.util.MathUtil;
 
 import java.math.RoundingMode;
 import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.List;
 
 public class solvedDOEoneFactor extends AppCompatActivity {
 
@@ -36,6 +47,13 @@ public class solvedDOEoneFactor extends AppCompatActivity {
     TextView anovaf1;
     TextView fishersign;
     TextView conftv;
+    Button sel1;
+    Button sel2;
+    AlertDialog dialog;
+    AlertDialog.Builder builder;
+    AlertDialog dialog2;
+    AlertDialog.Builder builder2;
+    TextView icMeanDifValue;
     Gson gson = new GsonBuilder().create();
 
     @Override
@@ -110,6 +128,7 @@ public class solvedDOEoneFactor extends AppCompatActivity {
 
         LinearLayout baselayout = findViewById(R.id.starterlayoutDOE);
         int width = (int) getResources().getDimension(R.dimen.widthstate3); //obtiene manualmente el width 58 de dimens
+        int width3 = (int) getResources().getDimension(R.dimen.widthinput);
         int width2 = (int) getResources().getDimension(R.dimen.width); //obtiene manualmente el width 58 de dimens
         int height = (int) getResources().getDimension(R.dimen.height); //obtiene manualmente el height 35 de dimens
         int height2 = (int) getResources().getDimension(R.dimen.height2);
@@ -184,8 +203,143 @@ public class solvedDOEoneFactor extends AppCompatActivity {
 
         }
 
+        LinearLayout baselayout2 = findViewById(R.id.LSDlinearlayout);
+        LinearLayout baselayout3 = findViewById(R.id.LSDlinearlayout2);
+        Combinations c = new Combinations(avalueint,2);
+        List<int[]> al = new ArrayList<>();
+        for(int[] iterate : c){
+            al.add(iterate);
+        }
+
+        for (int i = 0 ; i < al.size(); i++){
+            int[] comb = al.get(i);
+            int v1 = comb[0];
+            int v2 = comb[1];
+            double op = averagePerTreatArray[v1] - averagePerTreatArray[v2];
+            TextView tv = new TextView(solvedDOEoneFactor.this);
+            tv.setTextSize(textsize);
+            tv.setTypeface(Typeface.DEFAULT_BOLD);
+            String plchldr = df.format(averagePerTreatArray[v1]) + " - " +  df.format(averagePerTreatArray[v2]) + " = " + df.format(op);
+            tv.setText(plchldr);
+            tv.setId(i);
+            tv.setTextColor(Color.WHITE);
+            tv.setBackgroundResource(R.drawable.rect);
+            tv.setGravity(Gravity.CENTER | Gravity.CENTER_VERTICAL | Gravity.CENTER_HORIZONTAL);
+            LinearLayout.LayoutParams params2 = new LinearLayout.LayoutParams(width3, height); //58, 35
+            params2.setMargins(0, 0, 0, topmargin);
+            tv.setLayoutParams(params2);
+            baselayout3.addView(tv);
+
+            TextView tv2 = new TextView(solvedDOEoneFactor.this);
+            tv2.setTextSize(textsize);
+            tv2.setTypeface(Typeface.DEFAULT_BOLD);
+            tv2.setText(Html.fromHtml("a<sub><small>" + v1 + "</small></sub> vs a<sub><small>" + v2 + "</small></sub>"));
+            tv2.setId(i);
+            tv2.setTextColor(Color.WHITE);
+            tv2.setBackgroundResource(R.drawable.rect);
+            tv2.setGravity(Gravity.CENTER | Gravity.CENTER_VERTICAL | Gravity.CENTER_HORIZONTAL);
+            LinearLayout.LayoutParams params3 = new LinearLayout.LayoutParams(width3, height); //58, 35
+            params3.setMargins(0, 0, 0, topmargin);
+            tv2.setLayoutParams(params3);
+            baselayout2.addView(tv2);
+        }
 
 
 
+
+
+
+        icMeanDifValue = findViewById(R.id.tvICMeanDifValue);
+        sel1 = findViewById(R.id.treatSel1);
+        sel2 = findViewById(R.id.treatSel2);
+        String pos = "";
+        sel1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sel2.setEnabled(false);
+                sel2.setText(R.string.sel);
+                icMeanDifValue.setText(R.string.icmeandifTEMP);
+                builder = new AlertDialog.Builder(solvedDOEoneFactor.this);
+                builder.setTitle(getString(R.string.seltitle));
+
+                final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+                        solvedDOEoneFactor.this,
+                        R.layout.listpicker);
+
+                for (int i = 0; i < avalueint; i++) {
+                    arrayAdapter.add(String.valueOf(i+1));
+                }
+
+                builder.setNegativeButton(getString(R.string.cancelkey), (dialog, which) -> dialog.dismiss());
+
+                builder.setAdapter(arrayAdapter,
+                        (dialog, which) -> {
+
+                            String pos = arrayAdapter.getItem(which);
+                            sel1.setText(pos);
+                            sel2.setEnabled(true);
+                            sel2.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    builder2 = new AlertDialog.Builder(solvedDOEoneFactor.this);
+                                    builder2.setTitle(getString(R.string.seltitle));
+                                    builder2.setNegativeButton(getString(R.string.cancelkey), (dialog, which) -> dialog.dismiss());
+                                    arrayAdapter.remove(pos);
+                                    builder2.setAdapter(arrayAdapter,
+                                            (dialog, which) -> {
+
+                                                String pos = arrayAdapter.getItem(which);
+                                                sel2.setText(pos);
+                                                sel2.setEnabled(true);
+
+                                                String selstr1 = sel1.getText().toString();
+                                                String selstr2 = sel2.getText().toString();
+                                                int sel1int = Integer.parseInt(selstr1) - 1;
+                                                int sel2int = Integer.parseInt(selstr2) - 1;
+                                                double v1 = tvalue * Math.sqrt((2 * mc2) / nvalueint);
+                                                double icop1 = averagePerTreatArray[sel1int] - averagePerTreatArray[sel2int] - v1;
+                                                double icop2 = averagePerTreatArray[sel1int] - averagePerTreatArray[sel2int] + v1;
+
+                                                icMeanDifValue.setText(Html.fromHtml(df.format(icop1) + " &#8804; &#956;<sub><small>" + selstr1 +"</small></sub> - &#956;<sub><small>"+ selstr2 + "</small></sub> &#8804; " + df.format(icop2)));
+
+
+
+
+                                                dialog.dismiss();
+                                            });
+                                    dialog2 = builder2.create();
+                                    dialog2.show();
+
+                                    Button NegativeBtn2 = dialog2.getButton(DialogInterface.BUTTON_NEGATIVE);
+                                    NegativeBtn2.setBackgroundColor(Color.parseColor("#5865F2"));
+                                    NegativeBtn2.setTextColor(Color.BLACK);
+                                    ViewGroup.MarginLayoutParams params2 = (ViewGroup.MarginLayoutParams)NegativeBtn2.getLayoutParams();
+                                    params2.topMargin = (int) getResources().getDimension(R.dimen.margin);
+                                    params2.leftMargin = (int) getResources().getDimension(R.dimen.margin);
+                                    params2.rightMargin = (int) getResources().getDimension(R.dimen.margin);
+                                    NegativeBtn2.setLayoutParams(params2);
+
+                                }
+
+
+                            });
+                            dialog.dismiss();
+                        });
+
+                dialog = builder.create();
+                dialog.show();
+
+                Button NegativeBtn = dialog.getButton(DialogInterface.BUTTON_NEGATIVE);
+                NegativeBtn.setBackgroundColor(Color.parseColor("#5865F2"));
+                NegativeBtn.setTextColor(Color.BLACK);
+                ViewGroup.MarginLayoutParams params2 = (ViewGroup.MarginLayoutParams)NegativeBtn.getLayoutParams();
+                params2.topMargin = (int) getResources().getDimension(R.dimen.margin);
+                params2.leftMargin = (int) getResources().getDimension(R.dimen.margin);
+                params2.rightMargin = (int) getResources().getDimension(R.dimen.margin);
+                NegativeBtn.setLayoutParams(params2);
+            }
+        });
     }
+
+
 }
